a4f4eb2475640691184b15c9563df57700abc8d6
THD <ok821000@gmail.com>
2025-06-20 15:14:17 +0900

    feat: Set User domain header, footer
    - 유저 도메인의 header, footer 분리

--- src/main/java/com/yesul/chatroom/model/entity/Chatroom.java
+++ src/main/java/com/yesul/chatroom/model/entity/Chatroom.java
@@ -1,7 +1,19 @@
 package com.yesul.chatroom.model.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Chatroom {
+
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
