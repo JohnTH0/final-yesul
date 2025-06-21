9e4868230e04ca804b510dd011dd4d898dc36e62
TH <ok821000@gmail.com>
2025-06-21 22:33:41 +0900

    fix : Fix BaseTimeEntity
    - BaseTimeEntity 누락된 애너테이션 추가

--- src/main/java/com/yesul/YesulApplication.java
+++ src/main/java/com/yesul/YesulApplication.java
@@ -2,8 +2,10 @@ package com.yesul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class YesulApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/yesul/common/BaseTimeEntity.java
+++ src/main/java/com/yesul/common/BaseTimeEntity.java
@@ -1,12 +1,14 @@
 package com.yesul.common;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
@@ -14,8 +16,10 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public class BaseTimeEntity {
 
     @CreatedDate
+    @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 }
